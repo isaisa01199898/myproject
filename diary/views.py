0615cd76a7f65c2a5e12ec15d86e4201a0b6bbe7
isaisa01199898 <isamikonoue0119@gmail.com>
2025-03-25 15:06:24 +0900

    Update views.py

--- diary/views.py
+++ diary/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import View
 from datetime import datetime
 from zoneinfo import ZoneInfo
+from openai import OpenAI
 ser_data=40
 data1=100
 data2=40
@@ -10,6 +11,19 @@ data4=60
 data5=30
 data6=88
 data7=68
+
+client = OpenAI(api_key="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")
+completion = client.chat.completions.create(
+    model="gpt-4o-mini",
+    messages=[
+        {"role": "system", "content": "You are a poetic assistant, skilled in explaining complex programming concepts with creative flair."},
+        {"role": "user", "content": "Compose a poem that explains the concept of recursion in programming."}
+  ]
+)
+
+print(completion.choices[0].message)
+
+
 
 class IndexView(View):
     def get(self, request):
@@ -24,6 +38,7 @@ class IndexView(View):
 "data5":data5,
 "data6":data6,
 "data7":data7
+"message":respons
 })  
  
 
